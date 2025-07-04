80ea430b39ba5d134b3054c69c482ad7069c882d
Thimeth0013 <sathmikakb@gmail.com>
2025-07-04 12:31:42 +0530

    Updated App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -12,6 +15,9 @@ const App = () => {
         <Navbar />
         <Hero/>
         <Cocktails />
+        <About />
+        <Art/>
+        <Contact />
     </main>
   )
 }
